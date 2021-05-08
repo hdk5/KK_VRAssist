bfcd8cbdeadfd0594206b7d00b9f306664ce31d0
MayouKurayami <mkw801021@gmail.com>
2021-05-08 16:00:17 -0700

    Add BepInProcess, use Awake() instead of constructor Refactor Harmony patching

--- KoikatuVRAssistPlugin/KoikatuVRAssistPlugin.cs
+++ KoikatuVRAssistPlugin/KoikatuVRAssistPlugin.cs
@@ -5,22 +5,36 @@ using System;
 namespace KoikatuVRAssistPlugin
 {
 	[BepInPlugin(GUID, PluginName, Version)]
+	[BepInProcess("KoikatuVR")]
+	[BepInProcess("Koikatsu Party VR")]
 	public class KoikatuVRAssistPlugin : BaseUnityPlugin
 	{
 		public const string GUID = "KK_KoikatuVRAssistPlugin";
 		public const string Version = "1.1.0";
 		public const string PluginName = "KoikatuVRAssistPlugin";
 
-		public KoikatuVRAssistPlugin()
+		private void Awake()
 		{
 			if (Type.GetType("VRHScene, Assembly-CSharp") != null)
 			{
-				Logger.LogMessage("Installs Hook");
-				Harmony.CreateAndPatchAll(typeof(GripMoveHook), "KoikatuVRAssistPlugin.GripMoveHook");
+				var harmony = new Harmony(GUID);
+				try
+				{
+					harmony.PatchAll(typeof(GripMoveHook));
+				}
+				catch (Exception)
+				{
+					harmony.UnpatchAll(harmony.Id);
+					Logger.LogError("Harmony patch failed, Nothing Patched.");
+					throw;
+				}		
+#if DEBUG
+				Logger.LogDebug("Hooks Patched");
+#endif
 			}
 			else
 			{
-				Logger.LogMessage("Not KoikatuVR. Shutdown KoikatuVRAssistPlugin.");
+				Logger.LogError("VRHScene not found. Nothing Patched.");
 			}
 		}
 	}
